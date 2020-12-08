6adc4a46b503d930b3794642c0e4eb54d246cd37
Sunil <sunilsv26@gmail.com>
2020-12-08 16:10:19 +0530

    #15 fetching user location using navigation object,connected to redux store,dispatched actions to get user location

--- src/containers/search/search.js
+++ src/containers/search/search.js
@@ -1,20 +1,57 @@
 //Component with state which responsible for input,sevenDay report
 
 import React, { Component, Fragment } from 'react';
+import {connect} from 'react-redux';
 
-import SearchBar from '../../components/searchBar/searchBar'
+import SearchBar from '../../components/searchBar/searchBar';
+import CityName from '../../components/searchResult/cityName/cityName';
+import * as actions from '../../store/actions/search'
 
 class Search extends Component{
     state={
         input:'',
+        error:null,
+    }
+
+    componentDidMount(){
+        navigator.geolocation.getCurrentPosition((position)=>{
+            let lat= position.coords.latitude;
+            let lon=position.coords.longitude;
+            this.props.onGetUserLocation(lat,lon);
+            
+        },
+        (err)=>this.setState({error:err}))
+        setTimeout(()=>{this.setState({input:this.props.city})},3000)
+    }
+
+    onChangeHandler=(event)=>{
+        let newInput = event.target.value;
+        this.setState({input:newInput})
     }
     render(){
         return(
             <Fragment>
-                <SearchBar />
+                <SearchBar value={this.state.input} onChange={this.onChangeHandler}/>
+                <CityName />
             </Fragment>
         )
     }
 }
+const mapStateToProps=state=>{
+    return{
+        city:state.search.city,
+        lat:state.search.lat,
+        lon:state.search.lon,
+        temp:state.search.temp,
+        weather:state.search.weather,
+        icon:state.search.icon,
+    }
+}
 
-export default Search;
+const mapDispatchToProps=dispatch=>{
+    return{
+        onGetUserLocation:(lat,lon)=>dispatch(actions.getUserCity(lat,lon))
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps) (Search);
