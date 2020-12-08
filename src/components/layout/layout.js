73c0fc1fec8f8dc27695522c871597380b7bc307
Sunil <sunilsv26@gmail.com>
2020-12-08 12:14:13 +0530

    #2 Defined new layout component

---
+++ src/components/layout/layout.js
@@ -0,0 +1,12 @@
+import classes from './layout.module.css';
+import React from 'react';
+
+const layout=(props)=>{
+    return(
+        <div className={classes.Layout}>
+        
+        </div>
+    )
+}
+
+export default layout
