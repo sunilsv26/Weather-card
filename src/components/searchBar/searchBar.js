441587b0d9192bfa757e0065e3218d31395e8a4b
Sunil <sunilsv26@gmail.com>
2020-12-08 13:18:33 +0530

    #9 Added icons in input area

--- src/components/searchBar/searchBar.js
+++ src/components/searchBar/searchBar.js
@@ -2,13 +2,17 @@
 
 import React from 'react';
 import classes from './searchBar.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMapMarkerAlt, faSearch} from '@fortawesome/free-solid-svg-icons'
 
 const searchbar=(props)=>{
     return(
-        <div>
+        <div className={classes.SearchBar}>
+            <FontAwesomeIcon icon={faMapMarkerAlt} className={classes.LocationTag} />
             <input className={classes.SearchField}
             placeholder='City Name' value={props.value}
             onChange={(event)=>props.onChange(event)} />
+            <FontAwesomeIcon icon={faSearch} className={classes.SearchIcon} />
         </div>
     )
 }
