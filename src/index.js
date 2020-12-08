fdbf9f2d670b0de04ee952ee79d015790318ffa5
Sunil <sunilsv26@gmail.com>
2020-12-08 14:02:34 +0530

    #12 Initial setup for redux

--- src/index.js
+++ src/index.js
@@ -4,9 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {Provider} from 'react-redux';
+import {createStore,combineReducers,applyMiddleware,compose} from 'redux';
+import thunk from 'redux-thunk';
+import searchReducer from './store/reducers/searchReducer'
+
+const rootReducer=combineReducers({
+  search:searchReducer,
+})
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store =createStore(rootReducer,composeEnhancers(applyMiddleware(thunk)) )
+
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
