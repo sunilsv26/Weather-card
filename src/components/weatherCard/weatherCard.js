011c029574e9036658d43bb9f1b15f61d8824cbe
Sunil <sunilsv26@gmail.com>
2020-12-08 12:40:50 +0530

    #6 Defined and rendered weathercard component

---
+++ src/components/weatherCard/weatherCard.js
@@ -0,0 +1,12 @@
+//Card Component which holds input,result,weatherdata
+
+import React from "react";
+
+import classes from "./weatherCard.module.css";
+
+
+const weatherCard = (props) => {
+  return <div className={classes.WeatherCard}></div>;
+};
+
+export default weatherCard
