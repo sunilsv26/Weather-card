0430653cc5d1cdaaf65141828ca5d05331ccc66d
Sunil <sunilsv26@gmail.com>
2020-12-09 11:31:02 +0530

    #34 Added day prop and css props

--- src/components/detailedForecast/tempAndDay/tempAndDay.js
+++ src/components/detailedForecast/tempAndDay/tempAndDay.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import classes from './tempAndDay.module.css';
+import React from "react";
+import classes from "./tempAndDay.module.css";
 
-const tempAndDay=(props)=>{
-    return(
-        <div className={classes.TempAndDay}>
-            <div className={classes.Temp}>{(props.temp-273.15).toFixed(2)}&#8451;</div>
-            <img src={`http://openweathermap.org/img/wn/${props.icon}@2x.png`} alt='temp' />
-            <div>{props.day},{props.date},{props.month}</div>
-            <div>{props.city}</div>
-        </div>
-    )
-}
+const tempAndDay = (props) => {
+  return (
+    <div className={classes.TempAndDay}>
+      <div className={classes.Temp}>
+        {(props.temp - 273.15).toFixed(2)}&#8451;
+      </div>
+      <img
+        src={`http://openweathermap.org/img/wn/${props.icon}@2x.png`}
+        alt="temp"
+      />
+      <div className={classes.Day}>
+        {props.day}
+        <div>{props.city}</div>
+      </div>
+    </div>
+  );
+};
 
 export default tempAndDay;
