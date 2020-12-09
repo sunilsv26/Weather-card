14dfb338fc31cd07d89985c30fd88ef74c1b9f92
Sunil <sunilsv26@gmail.com>
2020-12-09 11:28:52 +0530

    #33 Imported dayAndTemp comp and passed day and temp props

--- src/components/detailedForecast/detailedForecast.js
+++ src/components/detailedForecast/detailedForecast.js
@@ -3,11 +3,15 @@
 import React from "react";
 import { connect } from "react-redux";
 import classes from "./detailedForecast.module.css";
-import PressureAndHumidity from './pressureAndHumidity/pressureAndHumidity'
+import PressureAndHumidity from './pressureAndHumidity/pressureAndHumidity';
+import TempAndDay from './tempAndDay/tempAndDay';
 
 const detailedForecast=(props)=>{
     return(
         <div className={classes.DetailedForecast}>
+            <TempAndDay temp={props.temp} icon={props.icon}
+            day={props.day} city={props.city}
+            />
             <PressureAndHumidity pressure={props.pressure} humidity={props.humidity}/>
         </div>
     )
@@ -21,6 +25,8 @@ const mapStateToProps=state=>{
     humidity:state.sevenDayReport.humidity,
     sunrise:state.sevenDayReport.sunrise,
     sunset:state.sevenDayReport.sunset,
+    day:state.sevenDayReport.day,
+    city:state.search.city,
     }
 }
 
