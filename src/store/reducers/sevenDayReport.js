13321711a4691ff79f1497ef2dedc5c6f01b9b1c
Sunil <sunilsv26@gmail.com>
2020-12-09 10:26:16 +0530

    #27 Defined sevenDayreport reducer

---
+++ src/store/reducers/sevenDayReport.js
@@ -0,0 +1,28 @@
+import * as actiontypes from '../actions/actionTypes';
+
+let initialState= {
+    temp:'',
+    icon:'',
+    pressure:'',
+    humidity:'',
+    sunrise:'',
+    sunset:'',
+}
+
+const reducer =(state=initialState,action)=>{
+    switch (action.type) {
+        case actiontypes.ON_DAY_SELECTION:return{
+            ...state,
+            temp:action.temp,
+            icon:action.icon,
+            pressure:action.pressure,
+            humidity:action.humidity,
+            sunrise:action.sunrise,
+            sunset:action.sunset,
+        }
+        default:
+            return state;
+    }
+}
+
+export default reducer;
